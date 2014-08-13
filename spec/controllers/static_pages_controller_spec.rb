e9962e554aa0a821817419038550d066064874fd
Your Name <your.email@example.com>
2014-08-13 15:35:07 +0300

    Add a StaticPages controller

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
+
+  describe "GET ..." do
+    it "returns http success" do
+      get :...
+      expect(response).to be_success
+    end
+  end
+
+end
