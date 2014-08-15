de5e3f571610670275af4bd4fb6d248c72efdd6d
Your Name <your.email@example.com>
2014-08-15 15:11:04 +0300

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -72,7 +72,8 @@ RSpec.configure do |config|
 
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended.
-    mocks.verify_partial_doubles = true
+    mocks.verify_partial_doubles = true1
+    config.include Capybara::DSL
   end
 =end
 end
