68c2764287a361152757b238fbc923acd83c35a8
Sapozhnikov, Andrey <skarm61@gmail.com>
2014-08-16 19:43:51 +0300

    Fix for tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, :type => :controller do
 
-  describe "GET ..." do
-    it "returns http success" do
-      get :...
-      expect(response).to be_success
-    end
-  end
-
 end
